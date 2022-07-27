c6a7176674e1ea529732b77ca3280b738b9f94d0
ryo072144 <84168405+ryo072144@users.noreply.github.com>
2022-07-27 13:48:58 +0900

    Update widget.dart

--- lib/widget.dart
+++ lib/widget.dart
@@ -1,6 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-TextStyle titleStyle(){
-  return const TextStyle(color: Color(0xffeeeeee), fontWeight: FontWeight.bold, fontSize: 20);
-}
+const TextStyle titleStyle = TextStyle(color: Color(0xffeeeeee), fontSize: 20 ,fontWeight: FontWeight.bold);
